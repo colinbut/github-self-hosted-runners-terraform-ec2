9cd575a52bf0f084ef0d130be5121e4c663175bd
Colin But <colin.but@outlook.com>
2021-07-15 21:57:24 +0100

    chore: formatting

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "aws" {
 resource "aws_instance" "github_runner" {
   ami           = var.ami
   instance_type = var.instance_type
-  key_name = var.key_name
+  key_name      = var.key_name
 
   tags = {
     Name = "github_runner"
--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "ami" {
   description = "The AMI for the GitHub Runner backing EC2 Instance"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
   description = "The type of the EC2 instance backing the GitHub Runner"
-  type = string
+  type        = string
 }
 
 variable "key_name" {
   description = "The KeyPair name for accessing (SSH) into the EC2 instance backing the GitHub Runner"
-  type = string
+  type        = string
 }
