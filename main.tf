a602396006042a347bc1caa8c8b43c5b58836a73
Colin But <colin.but@outlook.com>
2021-07-15 21:55:08 +0100

    feat: main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,25 @@
 terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 3.27"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-west-2"
+}
+
+resource "aws_instance" "github_runner" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name = var.key_name
+
+  tags = {
+    Name = "github_runner"
+  }
 }
