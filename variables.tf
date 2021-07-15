a7ecad51bdcdd2f3848b520d86db684825754cda
Colin But <colin.but@outlook.com>
2021-07-15 21:55:24 +0100

    feat: adding in required variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "ami" {
+  description = "The AMI for the GitHub Runner backing EC2 Instance"
+  type = string
+}
+
+variable "instance_type" {
+  description = "The type of the EC2 instance backing the GitHub Runner"
+  type = string
+}
+
+variable "key_name" {
+  description = "The KeyPair name for accessing (SSH) into the EC2 instance backing the GitHub Runner"
+  type = string
+}
