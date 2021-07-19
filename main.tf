3810836744b245de70f55348ca9be52a05193413
Colin But <colin.but@outlook.com>
2021-07-19 20:33:02 +0100

    feat: switch to use EC2 AutoScaling Groups

--- main.tf
+++ main.tf
@@ -14,12 +14,24 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-resource "aws_instance" "github_runner" {
-  ami           = var.ami
+resource "aws_launch_template" "name" {
+  name        = "github_runner_launch_template"
+  description = "Launch Template for GitHub Runners EC2 AutoScaling Group"
+
+  block_device_mappings {
+    device_name = "/dev/sda1"
+
+    ebs {
+      delete_on_termination = true
+      volume_size           = 20
+    }
+  }
+
+  image_id      = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  user_data = templatefile("${path.cwd}/bootstrap.tmpl", { github_repo_url = var.github_repo_url, github_repo_pat_token = var.github_repo_pat_token, runner_name = var.runner_name, labels = join(",",var.labels) })
+  user_data = templatefile("${path.cwd}/bootstrap.tmpl", { github_repo_url = var.github_repo_url, github_repo_pat_token = var.github_repo_pat_token, runner_name = var.runner_name, labels = join(",", var.labels) })
 
   tags = {
     Name = "github_runner"
