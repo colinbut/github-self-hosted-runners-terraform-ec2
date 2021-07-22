55bf9f56cb766ebd90eb80d874933e31fc4e658c
Colin But <colin.but@outlook.com>
2021-07-22 22:47:16 +0100

    feat: autoscaling group

--- main.tf
+++ main.tf
@@ -14,26 +14,37 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
-resource "aws_launch_template" "name" {
+
+data "aws_availability_zones" "all_azs" {
+  state = "available"
+}
+
+
+resource "aws_launch_template" "ec2_launch_template" {
   name        = "github_runner_launch_template"
   description = "Launch Template for GitHub Runners EC2 AutoScaling Group"
-
-  block_device_mappings {
-    device_name = "/dev/sda1"
-
-    ebs {
-      delete_on_termination = true
-      volume_size           = 20
-    }
-  }
 
   image_id      = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  user_data = templatefile("${path.cwd}/bootstrap.tmpl", { github_repo_url = var.github_repo_url, github_repo_pat_token = var.github_repo_pat_token, runner_name = var.runner_name, labels = join(",", var.labels) })
+  user_data = base64encode(templatefile("${path.cwd}/bootstrap.tmpl", { github_repo_url = var.github_repo_url, github_repo_pat_token = var.github_repo_pat_token, runner_name = var.runner_name, labels = join(",", var.labels) }))
 
   tags = {
     Name = "github_runner"
+  }
+}
+
+resource "aws_autoscaling_group" "github_runners_autoscaling_group" {
+  name                      = "github_runners_autoscaling_group"
+  availability_zones        = data.aws_availability_zones.all_azs.names
+  health_check_type         = "EC2"
+  health_check_grace_period = 600
+  desired_capacity          = 3
+  min_size                  = 1
+  max_size                  = 4
+  launch_template {
+    id      = aws_launch_template.ec2_launch_template.id
+    version = "$Latest"
   }
 }
